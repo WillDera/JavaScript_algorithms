fe3eafef68a2901deac40f1a7c013117061e8aa5
Godswill <ezeokewill@gmail.com>
2020-03-24 00:17:17 +0100

    New Commit
    Added another algorithm.

---
+++ Find and Delete/findAndDelete.js
@@ -0,0 +1,16 @@
+/*
+	This piece of code takes an array(first argument), followed by one or more arguments. It then Removes all elements from the array that are of the same values as the arguments.
+ */
+
+function deleter(arr) {
+  var args = Array.prototype.slice.call(arguments);
+
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < args.length; j++) {
+      if (arr[i] === args[j]) {
+        delete arr[i];
+      }
+    }
+  }
+  return arr.filter(Boolean);
+}
