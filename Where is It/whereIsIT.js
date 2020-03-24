2cd265b815859abba81f3a154ecf57e7cf35c678
Godswill <ezeokewill@gmail.com>
2020-03-24 22:55:05 +0100

    Where is IT
    A function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs with the second argument.

---
+++ Where is It/whereIsIT.js
@@ -0,0 +1,29 @@
+function whatIsIT(collection, source) {
+  // "What's in a name? that which we call a rose
+  // By any other name would smell as sweet.”
+  // -- by William Shakespeare, Romeo and Juliet
+  var srcKeys = Object.keys(source);
+
+  // filter the collection
+  return collection.filter(function(obj) {
+    for (var i = 0; i < srcKeys.length; i++) {
+      if (
+        !obj.hasOwnProperty(srcKeys[i]) ||
+        obj[srcKeys[i]] !== source[srcKeys[i]]
+      ) {
+        return false;
+      }
+    }
+    return true;
+  });
+}
+
+// here's a test
+whereIsIT(
+  [
+    { first: "Romeo", last: "Montague" },
+    { first: "Mercutio", last: null },
+    { first: "Tybalt", last: "Capulet" }
+  ],
+  { last: "Capulet" }
+);
